8a72d918f497b070069fa3c1383116036a4236ac
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-22 11:57:40 +0530

    Javaprogram

---
+++ src/SnakeladderSingleplayer/Singleplayer.java
@@ -0,0 +1,9 @@
+package SnakeladderSingleplayer;
+
+public class Singleplayer {
+	public static void main(String[] args) {
+		       int start = 0;
+		       System.out.println("Starting_Position : "+start);
+	}
+}
+
