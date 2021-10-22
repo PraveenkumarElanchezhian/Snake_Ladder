a39bb4cfdf06615c954870863d159ec77b5edd57
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-22 12:07:24 +0530

    Javaprogram

--- src/SnakeladderSingleplayer/Singleplayer.java
+++ src/SnakeladderSingleplayer/Singleplayer.java
@@ -6,8 +6,18 @@ public class Singleplayer {
 		       System.out.println("Starting_Position : "+start);
 		       int min = 1;
 			   int max = 6;
+			   int movecount = 0;
 			 int random = (int) Math.floor(Math.random()*(max-min+1)+min);
 			 System.out.println("Die_Random_value : "+random);
+			 
+			 if (movecount == 0) 
+			  { 
+				  System.out.println("player in same position"); 
+			  } 
+			  else 
+			  {
+			      System.out.println("player in next position"); 
+			  }
 	}
 }
 
